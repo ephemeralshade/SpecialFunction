9b49e9fc197db871baebe5cc9ff5757c68d2d06e
ephemeralshade <tanaka524287@gmail.com>
2021-08-29 09:32:22 +0900

    GaussODE Frobenius m=3, 6

--- program/GaussODE.py
+++ program/GaussODE.py
@@ -1,4 +1,8 @@
 import sympy as sp
+import time
+import random
+
+start_time = time.time()
 
 (a, b, c, m, n, k) = sp.var('a, b, c, m, n, k')
 z = sp.Symbol('z')
@@ -16,17 +20,24 @@ def H(n):
 def w2(a, b, m, z):
     w1 = sp.hyper((a, b), [c], z)
     f1 = w1 * sp.ln(z) * (-1) ** (m - 1) * sp.rf(a - m, m) * sp.rf(b - m, m) / (sp.factorial(m) * sp.factorial(m - 1))
-    f2 = z ** (-m) * sp.summation(sp.rf(a - m, n) * sp.rf(b - m, n) / (sp.factorial(n) * sp.rf(1 - m, n)), (n, 0, m - 1))
+    f2 = z ** (-m) * sp.summation(sp.rf(a - m, n) * sp.rf(b - m, n) * z ** n / (sp.factorial(n) * sp.rf(1 - m, n)), (n, 0, m - 1))
     f3 = ((-1) ** (m - 1) / sp.factorial(m - 1)) * sp.summation(
-        (sp.rf(a - m, m) * sp.rf(b - m, m) * z ** (n - m) / (sp.factorial(n) * sp.factorial(n - m))) * (
-            H(m - 1) - H(n) - H(n - m) + sp.summation(1 / (a - m + k) + 1 / (b - m + k), (k, 0, n - 1))), (n, m, sp.oo))
-    return f3
+        (sp.rf(a - m, n) * sp.rf(b - m, n) * z ** (n - m) / (sp.factorial(n) * sp.factorial(n - m))) * (
+            H(m - 1) - H(n) - H(n - m) + sp.summation(1 / (a - m + k) + 1 / (b - m + k), (k, 0, n - 1))), (n, m, 64))
+    return f1 + f2 + f3
 
 
-a = 2
-b = 2
-m = 1
+m = 3
 c = 1 + m
-w = sp.hyper((a, b), [c], z)
-print(sp.simplify(L(a, b, c, w)))
-print(w2(a, b, m, z))
+W = w2(a, b, m, z)
+F = L(a, b, c, W)
+
+for _ in range(10):
+    a_ = random.uniform(-4, 4) * 4
+    b_ = random.uniform(-4, 4) * 4
+    print("a:", a_, " b:", b_)
+    for p in range(2, 10):
+        print(F.subs([(a, a_), (b, b_), (z, 1 / p)]).evalf())
+    print("--------------------------------")
+
+print("time :", time.time() - start_time)
